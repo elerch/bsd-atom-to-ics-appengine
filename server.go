2573ea51502eb6bf3d75f3147403c2a4baab7b14
Emil Lerch <emil@lerch.org>
2015-05-04 23:14:16 +0000

    update server.go with soemthing that works

--- server.go
+++ server.go
@@ -1,16 +1,34 @@
 package hello
 
 import (
-    "net/http";
+    "net/http"
     "github.com/elerch/bsd-atom-to-ics"
+    "appengine"
+    "appengine/urlfetch"
 )
 
 func init() {
-    http.HandleFunc("/", handler);
+    http.HandleFunc("/", handler)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    bsdatomtoics.AtomToICS(bsdatomtoics.FetchBytes(false), w, false);
+    c := appengine.NewContext(r)
+    // GAE doesn't seem to like this cert
+    client := createClient(c, true)
+    atom, err := bsdatomtoics.FetchBytesWith(client)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    bsdatomtoics.AtomToICS(atom, w, false)
 }
 
+func createClient(context appengine.Context, allowInvalidServerCertificate bool) *http.Client {
+    return &http.Client{
+        Transport: &urlfetch.Transport{
+            Context:  context,
+            AllowInvalidServerCertificate: allowInvalidServerCertificate,
+        },
+    }
+}
 
