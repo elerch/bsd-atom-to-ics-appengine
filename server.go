1ce891c0e4980e8bbf893837552f57562a39b2ef
Emil Lerch <emil@lerch.org>
2015-05-08 16:12:28 +0000

    support for query string specification of school

--- server.go
+++ server.go
@@ -12,10 +12,14 @@ func init() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+    school := r.URL.Query().Get("s");
+    selectedSchool := school
+    if selectedSchool == "" { selectedSchool = "district" }
+    w.Header().Set("X-School", selectedSchool)
     c := appengine.NewContext(r)
     // GAE doesn't seem to like this cert
     client := createClient(c, true)
-    atom, err := bsdatomtoics.FetchBytesWith(client)
+    atom, err := bsdatomtoics.FetchBytesWith(client, school)
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
